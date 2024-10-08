eacd9805b00ea1aec2f4d719fdf0c801f7b65882
Rakshika14 <rakshikaanbu9@gmail.com>
2024-10-08 14:40:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>RAKSHIKA</h1>
+      <h3>Web Developer</h3>
+      <hr />
+
+      <h5>333,senthamangalam,thiruvarur</h5>
+      <h5>rakshikaanbu9@gmail.com</h5>
+      <h5>7806973597</h5>
+      <hr />
+      <h3>SUMMARY</h3>
+      <p>
+        Highly motivated and enthusiatic individual seeking a challenging role to kickstart my career proficient in foundation in datascience eager to learn ,adapt,and contributes to innovative projects.I am qualified and professional web developers.seeking an entry-level opportunity with an esteemed organization where I can utilize my skils and enhance learning in the field of work.capable of mastering new technologies.
+      </p>
+      <hr />
+      <h3>TECHNICAL SKILLS</h3>
+      <ul>
+        <li>Html</li>
+        <li>Css</li>
+        <li>Git</li>
+        <li>Github</li>
+      </ul>
+      <hr />
+      <h3>EDUCATION</h3>
+      <h4>Bachelor of Engineering</h4>
+      <div>
+        <h5>Vetri Vinayaha College Of Engineering And Technology</h5>
+      </div>
+      <hr />
+      <h3>SKILLS</h3>
+      <ul>
+        <li>Leadership</li>
+        <li>Discipline</li>
+        <li>Self-confident</li>
+      </ul>
+      <hr />
+      <h3>PROJECTS</h3>
+      <h4>portfolio</h4>
+      <div>
+        <p>
+          The portfolio is devloped using reactjs a papular javascript library for building user interface.Additional technology include HTML5,CSS3.
+          </p>
+      </div>
+      <hr />
+      <h3>CERTIFICATION</h3>
+      <h4>full Stack Development</h4>
+      <h5>NoviTech I Combatore</h5>
+
+    </div>
+  );
+}
+
+export default App;
